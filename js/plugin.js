559c791ffaef3185a564659dfa5a99149761b862
missydove <missydovefitness@gmail.com>
2017-03-22 05:35:11 -0700

    i suuuck

--- js/plugin.js
+++ js/plugin.js
@@ -1,2 +1,15 @@
 // Paste jQuery Plugin Code Here
 
+var count = 0,
+    $svg = $('svg').drawsvg({
+      callback: function() {
+        $('#log').html('<p>Animation complete! <strong>' + (count++) + '</strong></p>');
+        animate();
+      }
+    });
+
+function animate() {
+  $svg.drawsvg('animate');  
+}
+
+animate();
